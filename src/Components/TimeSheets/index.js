6ce6dbee7ead03c3fb6bcf6693f546b243970f94
JuliMonte5 <julian.montenegro.1895@gmail.com>
2022-10-28 20:43:47 -0300

    TG-44: initial push with sample get

--- src/Components/TimeSheets/index.js
+++ src/Components/TimeSheets/index.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
 import styles from './time-sheets.module.css';
 
 function TimeSheets() {
+  const [TimeSheets, saveTimeSheets] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}api/timeSheet`);
+      const data = await response.json();
+      saveTimeSheets(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>TimeSheets</h2>
+      <div>
+        {TimeSheets.map((TimeSheet) => {
+          return <div key={TimeSheet._id}>{TimeSheet.description}</div>;
+        })}
+      </div>
     </section>
   );
 }
