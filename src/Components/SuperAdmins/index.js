100380d2b468160159b9e657c3862a8f2f064cfe
ProDIGGY-vinilos <cristiangerster@gmail.com>
2022-10-29 14:24:10 -0300

    TG-42: first commit and test get

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -1,9 +1,39 @@
 import styles from './super-admins.module.css';
+import { useEffect, useState } from 'react';
 
 function SuperAdmins() {
+  const [SuperAdmins, saveSuperAdmins] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}api/superAdmins`);
+      const data = await response.json();
+      saveSuperAdmins(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
   return (
     <section className={styles.container}>
       <h2>SuperAdmins</h2>
+      <div id="probando">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Last name</th>
+            <th>email</th>
+          </tr>
+          {SuperAdmins.map((SuperAdmin) => {
+            return (
+              <tr key={SuperAdmin._id}>
+                <td>{SuperAdmin.name}</td>
+                <td>{SuperAdmin.lastName}</td>
+                <td>{SuperAdmin.email}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </section>
   );
 }
