bae9e7895378c67b40f8c528d3907942533fbdf1
Ale <tiscorniaale@gmail.com>
2022-11-08 18:10:13 -0300

    TG-60:input component created

--- src/Components/Employees/EmployeeForm.js
+++ src/Components/Employees/EmployeeForm.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './employees.module.css';
 import Button from './Button';
+import InputField from '../Shared/input';
 
 const EmployeeForm = () => {
   const employeeId = useParams().id;
@@ -35,10 +36,7 @@ const EmployeeForm = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setUserInput((prev) => ({
-      ...prev,
-      [name]: value
-    }));
+    setUserInput({ ...userInput, [name]: value });
   };
 
   const onSubmit = async (e) => {
@@ -65,8 +63,8 @@ const EmployeeForm = () => {
       <form className={styles.container} onSubmit={onSubmit}>
         <h3 className={styles.titleForm}>Create Employee</h3>
         <div className={styles.formControl}>
-          <label>Name</label>
-          <input
+          <InputField
+            label="Name"
             name="name"
             type="text"
             placeholder="name"
@@ -75,8 +73,8 @@ const EmployeeForm = () => {
           />
         </div>
         <div className={styles.formControl}>
-          <label>Last Name</label>
-          <input
+          <InputField
+            label="Last Name"
             name="lastName"
             type="text"
             placeholder="last name"
@@ -85,8 +83,8 @@ const EmployeeForm = () => {
           />
         </div>
         <div className={styles.formControl}>
-          <label>Email</label>
-          <input
+          <InputField
+            label="Email"
             name="email"
             type="mail"
             placeholder="email"
@@ -95,8 +93,8 @@ const EmployeeForm = () => {
           />
         </div>
         <div className={styles.formControl}>
-          <label>Phone Number</label>
-          <input
+          <InputField
+            label="Phone Number"
             name="phone"
             type="number"
             placeholder="phone"
@@ -105,8 +103,8 @@ const EmployeeForm = () => {
           />
         </div>
         <div className={styles.formControl}>
-          <label>Password</label>
-          <input
+          <InputField
+            label="Password"
             name="password"
             type="password"
             placeholder="password"
