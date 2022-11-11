90d177875d189cc6fb744e417b8c112ab9b4ea81
fede230298 <federicoperaltaquinteros@gmail.com>
2022-11-11 12:00:35 -0300

    TG-79: ADD redux-devtools-extension, complete setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
-
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
