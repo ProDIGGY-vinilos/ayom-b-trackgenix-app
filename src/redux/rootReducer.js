54d3e673ed2c3a20cd60b7fc399e17e949f013c2
Ale <tiscorniaale@gmail.com>
2022-11-12 18:04:21 -0300

    TG-94: SuperAdmins redux - GET

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import timeSheetsReducer from './timeSheets/reducer';
+import superAdminsReducer from './superAdmins/reducer';
 
 const rootReducer = combineReducers({
-  timeSheets: timeSheetsReducer
+  timeSheets: timeSheetsReducer,
+  superAdmins: superAdminsReducer
 });
 
 export default rootReducer;
