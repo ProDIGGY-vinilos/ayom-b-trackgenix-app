4204e5598c4188117872390f67ce0b18b3e9b0e7
JuliMonte5 <julian.montenegro.1895@gmail.com>
2022-11-20 17:27:35 -0300

    TG-117: added custom messages for validations

--- src/Components/Tasks/Form/validations.js
+++ src/Components/Tasks/Form/validations.js
@@ -5,8 +5,15 @@ const schema = joi.object({
     .string()
     .required()
     .trim()
-    .regex(/^(?=.*[a-zA-Z].*)([\w\s\W]+)$/)
+    .pattern(/^(?=.*[a-zA-Z].*)([\w\s\W]+)$/)
     .min(3)
+    .messages({
+      'string.base': `"Description" must be a text`,
+      'string.empty': `"Description" cannot be empty`,
+      'string.min.base': `"Description" must have a minimum of 3 characters`,
+      'any.required': `"Description is required`,
+      'string.pattern.base': `"Description" can't have only numbers or special characters`
+    })
 });
 
 export default schema;
