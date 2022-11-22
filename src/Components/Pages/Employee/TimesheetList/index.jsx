54077900ad98c9e42c7b521783300967adcfb225
Maicol Freire <maicolfreire001@gmail.com>
2022-11-22 20:17:39 -0300

    TG-119: Removed unused buttons

--- src/Components/Pages/Employee/TimesheetList/index.jsx
+++ src/Components/Pages/Employee/TimesheetList/index.jsx
@@ -1,85 +1,34 @@
-import { useEffect /* , useState  */ } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getTimeSheetsByEmployee /* , deleteTimeSheet  */ } from 'redux/timeSheets/thunks';
+import { getTimeSheetsByEmployee } from 'redux/timeSheets/thunks';
 import styles from 'Components/TimeSheets/time-sheets.module.css';
-//import MessageModal from 'Components/Shared/Modal/MessageModal';
 import Table from 'Components/Shared/Table';
 import Button from 'Components/Shared/Button/Button';
 
 const TimeSheetsList = () => {
   const employeeId = '636c1e8ddabe537336ae082a';
-  /*   const [typeModal, setTypeModal] = useState('');
-  const [textModal, setTextModal] = useState('');
-  const [showMessageModal, setShowMessageModal] = useState(false); */
-  const { list: timeSheetsList, isLoading /* , error */ } = useSelector(
-    (state) => state.timeSheets
-  );
+  const { list: timeSheetsList, isLoading } = useSelector((state) => state.timeSheets);
   const dispatch = useDispatch();
-
-  /* const openMessageModal = () => {
-    setShowMessageModal(true);
-  };
-
-  const closeMessageModal = () => {
-    setShowMessageModal(false);
-  }; */
 
   useEffect(() => {
     dispatch(getTimeSheetsByEmployee(employeeId));
   }, []);
-
-  /* useEffect(async () => {
-    openModalOnError(error);
-  }, [error]); */
-
-  /*  const deleteTimeSheets = (id) => {
-    dispatch(deleteTimeSheet(id));
-    if (error) {
-      openModalOnError(error);
-    } else {
-      setTypeModal('Success');
-      setTextModal('TimeSheet was successfully removed');
-      openMessageModal();
-    }
-  }; */
 
   const columns = [
     { heading: 'Description', value: 'description' },
     { heading: 'Date', value: 'date', type: 'date' },
     { heading: 'Project', value: 'project', subValue: 'name' },
     { heading: 'Task', value: 'task', subValue: 'description' },
-    /* { heading: 'Employee', value: 'employee', subValue: 'lastName' }, */
-    { heading: 'Hours', value: 'hours' },
-    { heading: 'Actions' }
+    { heading: 'Hours', value: 'hours' }
   ];
-
-  /* const openModalOnError = (error) => {
-    if (error) {
-      setTypeModal('Error');
-      setTextModal(error);
-      openMessageModal();
-    }
-  }; */
 
   return (
     <section className={styles.container}>
-      <h2>Time Sheets</h2>
       {isLoading ? (
         <h3>Loading...</h3>
       ) : (
         <>
-          <Table
-            data={timeSheetsList}
-            columns={columns}
-            /* deleteItem={deleteTimeSheets} */
-            edit="/time-sheet-form"
-          />
-          {/* <MessageModal
-            type={typeModal}
-            isOpen={showMessageModal}
-            message={textModal}
-            handleClose={closeMessageModal}
-          /> */}
+          <Table data={timeSheetsList} columns={columns} />
           <Button href="/time-sheet-form" style="roundedPrimary" disabled={false} text="+" />
         </>
       )}
