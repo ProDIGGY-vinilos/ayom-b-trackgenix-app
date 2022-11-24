482f4495dfc80684412f8b134186cea301120b26
ProDIGGY-vinilos <cristiangerster@gmail.com>
2022-11-24 17:54:15 -0300

    TG-125: fixed datepicker and input invalid messages

--- src/Components/Shared/Datepicker/index.js
+++ src/Components/Shared/Datepicker/index.js
@@ -1,9 +1,11 @@
+import styles from 'Components/Shared/Datepicker/datePicker.module.css';
+
 const DatePicker = ({ label, inputName, register, error = '' }) => {
   return (
     <div>
       <label>{label}</label>
       <input {...register(inputName)} name={inputName} type="date"></input>
-      {error && <p>{error}</p>}
+      {error && <p className={styles.textColor}>{error}</p>}
     </div>
   );
 };
