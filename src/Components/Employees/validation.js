459963df70687b4deebfc34ce35ad5459fd7c949
Gonzalo Asencio <gestorgonzalo@gmail.com>
2022-11-24 16:34:28 -0300

    TG-126:added-employee-standaize-error-messages

--- src/Components/Employees/validation.js
+++ src/Components/Employees/validation.js
@@ -3,55 +3,55 @@ import Joi from 'joi';
 export const schema = Joi.object({
   name: Joi.string()
     .alphanum()
-    .pattern(/^([^0-9]*)$/i, 'only letters')
+    .pattern(/^([^0-9]*)$/i)
     .required()
     .messages({
-      'string.base': `"Name" should be a type of 'text'`,
-      'string.alphanum': `"Name" should be only characters`,
-      'string.empty': `"Name" cannot be an empty field`,
-      'string.pattern.name': `"Name" should be only letters`,
-      'any.required': `"Name" is a required field`
+      'string.base': 'Name must be of type text',
+      'string.alphanum': 'Name cannot contain special characters',
+      'string.empty': 'Name cannot be an empty field',
+      'string.pattern.base': 'Name should be only letters',
+      'any.required': 'Name is required'
     }),
   lastName: Joi.string()
     .alphanum()
-    .pattern(/^([^0-9]*)$/i, 'only letters')
+    .pattern(/^([^0-9]*)$/i)
     .required()
     .messages({
-      'string.base': `"Lastname" should be a type of 'text'`,
-      'string.alphanum': `"Lastname" should be only characters`,
-      'string.empty': `"Lastname" cannot be an empty field`,
-      'string.pattern.name': `"Lastname" should be only letters`,
-      'any.required': `"Lastname" is a required field`
+      'string.base': 'Last Name must be of type text',
+      'string.alphanum': 'Last Name cannot contain special characters',
+      'string.empty': 'Last Name cannot be an empty field',
+      'string.pattern.base': `"Lastname" should be only letters`,
+      'any.required': 'Last Name is required'
     }),
   phone: Joi.string()
     .length(10)
-    .pattern(/^[0-9]+$/, 'only numbers')
+    .pattern(/^[0-9]+$/)
     .required()
     .messages({
-      'string.base': `"Phone" should be a type of 'text'`,
-      'string.length': `"Phone" should be 10 numbers`,
-      'string.empty': `"Phone" cannot be an empty field`,
-      'string.pattern.name': `"Phone" should be only numbers`,
-      'any.required': `"Phone" is a required field`
+      'string.base': 'Phone must be of type number',
+      'string.length': 'Phone must be 10 characters long',
+      'string.empty': 'Phone cannot be an empty field',
+      'string.pattern.base': `"Phone" should be only numbers`,
+      'any.required': 'Phone is required'
     }),
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .required()
     .messages({
-      'string.base': `"Email" should be a type of 'text'`,
-      'string.email': `"Email" should be valid email format`,
-      'string.empty': `"Email" cannot be an empty field`,
-      'any.required': `"Name" is a required field`
+      'string.base': 'Email must be of type text',
+      'string.email': 'Invalid email format',
+      'string.empty': 'Email cannot be an empty field',
+      'any.required': 'Email is required'
     }),
   password: Joi.string()
     .alphanum()
-    .pattern(/^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/, 'Letters, numbers and minimum 8 characters')
+    .pattern(/^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/)
     .required()
     .messages({
-      'string.base': `"Password" should be a type of 'text'`,
-      'string.alphanum': `"Password" should be letters and numbers`,
-      'string.empty': `"Password" cannot be an empty field`,
-      'string.pattern.name': `"Password" should be letters, numbers and minimum 8 characters`,
-      'any.required': `"Password" is a required field`
+      'string.base': 'Password must be of type text',
+      'string.alphanum': 'Password cannot contain special characters',
+      'string.empty': 'Password cannot be an empty field',
+      'string.pattern.base': `"Password" should be letters, numbers and minimum 8 characters`,
+      'any.required': 'Password is required'
     })
 });
