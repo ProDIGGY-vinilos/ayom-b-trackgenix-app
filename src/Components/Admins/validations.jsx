201ac749e509ea01b55ae555970129116cca259b
Gonzalo Asencio <gestorgonzalo@gmail.com>
2022-11-24 16:18:07 -0300

    TG-126:added-admin-standaize-error-messages

--- src/Components/Admins/validations.jsx
+++ src/Components/Admins/validations.jsx
@@ -9,7 +9,7 @@ export const schema = Joi.object({
     .messages({
       'string.alphanum': 'Name cannot contain special characters.',
       'string.min': 'Name Length must be at least 3 characters long.',
-      'string.pattern.base': 'Name must have only letters.',
+      'string.pattern.base': 'Name must be of type string.',
       'string.empty': 'Name cannot be an empty field.',
       'any.required': 'Name is requiered.'
     }),
@@ -21,7 +21,7 @@ export const schema = Joi.object({
     .messages({
       'string.alphanum': 'Last Name cannot contain special characters.',
       'string.min': 'Last Name Length must be at least 3 characters long.',
-      'string.pattern.base': 'Last Name must have only letters.',
+      'string.pattern.base': 'Last Name must be of type string',
       'string.empty': 'Last Name cannot be an empty field.',
       'any.required': 'Last Name is requiered.'
     }),
@@ -29,7 +29,7 @@ export const schema = Joi.object({
     .email({ tlds: { allow: false } })
     .required()
     .messages({
-      'string.email': 'Invalid email.',
+      'string.email': 'Invalid email format.',
       'string.empty': 'Email cannot be an empty field.',
       'any.required': 'Email is requiered.'
     }),
