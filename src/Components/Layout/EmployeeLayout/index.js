e92b70c23d92e8c057293c28540033bad2c1bc0d
JuliMonte5 <julian.montenegro.1895@gmail.com>
2022-11-24 20:30:00 -0300

    TG-110: implemented lazy loading

--- src/Components/Layout/EmployeeLayout/index.js
+++ src/Components/Layout/EmployeeLayout/index.js
@@ -1,15 +1,14 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
+import createTitle from 'Helpers/create-title.js';
 import Header from 'Components/Shared/Header/index';
 import Sidebar from 'Components/Shared/Sidebar';
 import styles from 'Components/Layout/EmployeeLayout/layout.module.css';
-import ProjectsList from 'Components/Pages/Employee/ProjectList/index';
-import ProjectForm from 'Components/Projects/Form/index';
-import TimeSheets from 'Components/Pages/Employee/TimeSheetsList/index';
-import TimeSheetsForm from 'Components/Pages/Employee/TimeSheetsList/Form/index';
-import Home from 'Components/Home';
-import createTitle from 'Helpers/create-title.js';
+
+const ProjectsList = lazy(() => import('Components/Projects/index'));
+const TimeSheets = lazy(() => import('Components/TimeSheets/index'));
+const TimeSheetsForm = lazy(() => import('Components/TimeSheets/Form/index'));
 
 const Layout = () => {
   const sideBarOptions = [
@@ -25,17 +24,18 @@ const Layout = () => {
       <Header header={path} />
       <div className={styles.bodyContainer}>
         <Sidebar options={sideBarOptions} user={'Employee'} />
-        <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/employee/projects" component={ProjectsList} />
-          <Route exact path="/employee/timesheets" component={TimeSheets} />
-          <Route exact path="/employee/time-sheet-form" component={TimeSheetsForm} />
-          <Route path="/employee/time-sheet-form/:id" component={TimeSheetsForm} />
-          <Route path="/employee">
-            <Redirect to="/employee/projects" />
-          </Route>
-          {/* <Route exact path="/employee/account" component={} /> */}
-        </Switch>
+        <Suspense>
+          <Switch>
+            <Route exact path="/employee/projects" component={ProjectsList} />
+            <Route exact path="/employee/timesheets" component={TimeSheets} />
+            <Route exact path="/employee/time-sheet-form" component={TimeSheetsForm} />
+            <Route path="/employee/time-sheet-form/:id" component={TimeSheetsForm} />
+            <Route path="/employee">
+              <Redirect to="/employee/projects" />
+            </Route>
+            {/* <Route exact path="/employee/account" component={} /> */}
+          </Switch>
+        </Suspense>
       </div>
     </div>
   );
