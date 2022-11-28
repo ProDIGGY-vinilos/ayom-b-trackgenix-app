e2dbf3b608b257750b348bf972c7b5f2feb98618
Ale <tiscorniaale@gmail.com>
2022-11-28 10:41:51 -0300

    TG-134: sign up created

---
+++ src/Components/Pages/Auth/SignUp/index.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import MessageModal from 'Components/Shared/Modal/MessageModal';
+import styles from 'Components/Pages/Auth/SignUp/signup.module.css';
+import Button from 'Components/Shared/Button/Button';
+import InputField from 'Components/Shared/Input/input';
+import { useSelector, useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { postEmployee } from 'redux/employees/thunks';
+import { clearError } from 'redux/employees/actions';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { schema } from 'Components/Pages/Auth/SignUp/validation';
+
+const EmployeeForm = () => {
+  const dispatch = useDispatch();
+  const { error } = useSelector((state) => state.employees);
+  const [requested, setRequested] = useState(false);
+  const employeeId = useParams().id;
+  const employee = useSelector((state) =>
+    state.employees.list.find((employee) => employee._id === employeeId)
+  );
+
+  const [typeModal, setTypeModal] = useState('');
+  const [textModal, setTextModal] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm({
+    mode: 'onChange',
+    resolver: joiResolver(schema)
+  });
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    dispatch(clearError());
+  };
+
+  useEffect(() => {
+    if (error) {
+      setTypeModal('Error');
+      setTextModal(error);
+      openModal();
+      setRequested(false);
+    } else if (!employeeId && requested) {
+      setTextModal('Employee created successfully');
+      openModal();
+      setRequested(false);
+    }
+  }, [error]);
+
+  const employeeData = {
+    name: employee?.name,
+    lastName: employee?.lastName,
+    email: employee?.email,
+    phone: employee?.phone,
+    password: employee?.password
+  };
+
+  useEffect(() => {
+    dispatch(clearError());
+    if (employeeId) {
+      reset(employeeData);
+      return;
+    }
+  }, []);
+
+  const onSubmit = (data) => {
+    dispatch(postEmployee('', data));
+    setRequested(true);
+    setTypeModal('Success');
+  };
+
+  return (
+    <>
+      <h1 className={styles.title}>TRACKGENIX</h1>
+      <div className={styles.divForm}>
+        <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
+          <h3 className={styles.titleForm}>Sign Up</h3>
+          <div className={styles.formControl}>
+            <InputField
+              label="Name"
+              name="name"
+              type="text"
+              placeholder="name"
+              register={register}
+              error={errors.name?.message}
+            />
+          </div>
+          <div className={styles.formControl}>
+            <InputField
+              label="Last Name"
+              name="lastName"
+              type="text"
+              placeholder="last name"
+              register={register}
+              error={errors.lastName?.message}
+            />
+          </div>
+          <div className={styles.formControl}>
+            <InputField
+              label="Email"
+              name="email"
+              type="mail"
+              placeholder="email"
+              register={register}
+              error={errors.email?.message}
+            />
+          </div>
+          <div className={styles.formControl}>
+            <InputField
+              label="Phone Number"
+              name="phone"
+              type="number"
+              placeholder="phone"
+              register={register}
+              error={errors.phone?.message}
+            />
+          </div>
+          <div className={styles.formControl}>
+            <InputField
+              label="Password"
+              name="password"
+              type="password"
+              placeholder="password"
+              register={register}
+              error={errors.password?.message}
+            />
+          </div>
+          <div>
+            <MessageModal
+              type={typeModal}
+              isOpen={showModal}
+              message={textModal}
+              handleClose={closeModal}
+              goBack={'/login'}
+            />
+          </div>
+          <div className={styles.divBtn}>
+            <Button href="/home" style="squaredSecondary" disabled={false} text="Back" />
+            <Button
+              onClick={handleSubmit(onSubmit)}
+              style="squaredPrimary"
+              disabled={false}
+              text="Save"
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default EmployeeForm;
---
+++ src/Components/Pages/Auth/SignUp/validation.js
@@ -0,0 +1,63 @@
+import Joi from 'joi';
+
+export const schema = Joi.object({
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .pattern(/^([^0-9]*)$/i)
+    .required()
+    .messages({
+      'string.base': 'Name must be of type text',
+      'string.alphanum': 'Name cannot contain special characters',
+      'string.min': 'Name must be at least 3 characters long',
+      'string.empty': 'Name cannot be an empty field',
+      'string.pattern.base': 'Name must be of type text',
+      'any.required': 'Name is required'
+    }),
+  lastName: Joi.string()
+    .alphanum()
+    .min(3)
+    .pattern(/^([^0-9]*)$/i)
+    .required()
+    .messages({
+      'string.base': 'Last Name must be of type text',
+      'string.alphanum': 'Last Name cannot contain special characters',
+      'string.min': 'Last Name must be at least 3 characters long',
+      'string.empty': 'Last Name cannot be an empty field',
+      'string.pattern.base': 'Last Name must be of type text',
+      'any.required': 'Last Name is required'
+    }),
+  phone: Joi.string()
+    .length(10)
+    .pattern(/^[0-9]+$/)
+    .required()
+    .messages({
+      'string.base': 'Phone must be of type number',
+      'string.length': 'Phone must be 10 characters long',
+      'string.empty': 'Phone cannot be an empty field',
+      'string.pattern.base': 'Phone must be of type number',
+      'any.required': 'Phone is required'
+    }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .messages({
+      'string.base': 'Email must be of type text',
+      'string.email': 'Invalid email format',
+      'string.empty': 'Email cannot be an empty field',
+      'any.required': 'Email is required'
+    }),
+  password: Joi.string()
+    .alphanum()
+    .min(8)
+    .pattern(/^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/)
+    .required()
+    .messages({
+      'string.base': 'Password must be of type text',
+      'string.alphanum': 'Password cannot contain special characters',
+      'string.min': 'Password must be at least 8 characters long',
+      'string.empty': 'Password cannot be an empty field',
+      'string.pattern.base': 'Password must be of type text and number',
+      'any.required': 'Password is required'
+    })
+});
