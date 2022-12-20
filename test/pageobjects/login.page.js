2fc631f0f855944b027799338490f2ae4164d8c4
sofihoj <sofiahojberg@gmail.com>
2022-12-20 12:24:23 -0300

    TG-130: added login tests

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -4,19 +4,19 @@ class Login {
     }
 
     get inputMail () {
-        return $('#root > div > div > form > div:nth-child(1) > div > input[type=text]');
+        return $('#root > div > div.login_loginContainer__1LXOv > form > div:nth-child(1) > div > input[type=text]');
     }
 
     get inputPassword () {
-        return $('#root > div > div > form > div:nth-child(2) > div > input[type=password]');
+        return $('#root > div > div.login_loginContainer__1LXOv > form > div:nth-child(2) > div > div:nth-child(1) > input[type=password]');
     }
 
     get btnSubmit () {
-        return $('#root > div > div > form > div:nth-child(4) > button:nth-child(2)');
+        return $('#root > div > div.login_loginContainer__1LXOv > form > div.login_buttons__-_5bg > button');
     }
 
-    get btnBack () {
-        return $('#root > div > div > form > div:nth-child(4) > button:nth-child(1)')
+    get loginImg () {
+        return $('#root > div > div.login_loginImage__2cc5L');
     }
 
     get acceptBtn () {
@@ -24,19 +24,39 @@ class Login {
     }
 
     get emailMsg () {
-        return $('#root > div > div > form > div:nth-child(1) > div > p')
+        return $('#root > div > div.login_loginContainer__1LXOv > form > div:nth-child(1) > div > p')
     }
 
     get passwordMsg () {
-        return $('#root > div > div > form > div:nth-child(2) > div > p')
+        return $('#root > div > div.login_loginContainer__1LXOv > form > div:nth-child(2) > div > div:nth-child(1) > p')
+    }
+
+    get errorModal () {
+        return $('#root > div > div.login_loginContainer__1LXOv > form > div.login_buttons__-_5bg > div > div > div')
+    }
+
+    get modalBtn () {
+        return $('#root > div > div.login_loginContainer__1LXOv > form > div.login_buttons__-_5bg > div > div > button')
+    }
+
+    get successModal () {
+        return $('#root > div > div.login_loginContainer__1LXOv > form > div.login_buttons__-_5bg > div > div > div > h3')
     }
 
     get rrLogo () {
         return $('#root > div > header > img')
     }
 
-    async login (username, password) {
-        await this.inputMail.setValue(username);
+    get userType () {
+        return $('#root > div > div > aside > div.sidebar_bottom__3h20X > span')
+    }
+
+    get logOutBtn () {
+        return $('#root > div > div > aside > div.sidebar_bottom__3h20X > button')
+    }
+
+    async login (email, password) {
+        await this.inputMail.setValue(email);
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
     }
